f7032487ed6f52bab64aa1e146ab76b311af3921
sures <sures@Annam.bbrouter>
2024-08-08 19:59:38 +0530

    commit 2

--- HOMELYFSERVICES8AUG/src/DEMO/TEST1.java
+++ HOMELYFSERVICES8AUG/src/DEMO/TEST1.java
@@ -4,7 +4,8 @@ public class TEST1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println();
+System.out.println(1);
+System.out.println(2);
 	}
 
 }
